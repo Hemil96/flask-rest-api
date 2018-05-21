e2cf5a1fb38ef2b14ecb406ac223003fadc68654
Hemil96 <hemilpatel41196@gmail.com>
2018-05-21 23:09:31 +0530

    new methos added

--- flask-restful/app.py
+++ flask-restful/app.py
@@ -1,18 +1,26 @@
 from flask import Flask,request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate,identity
 
 app = Flask(__name__)
+app.secret_key = "hems"
 api = Api(app)
 
-items = [] 
+jwt = JWT(app, authenticate, identity) # /auth
+items = [{
+    "name":"soap",
+    "price":20
+}]
 
 class Item(Resource):
+    @jwt_required()
     def get(self ,name):
         item = next(filter(lambda x:x["name"]==name,items), None)
-        return {"item": None}, 200 if item else 404
+        return {"item": item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x:x["name"]==name,items), None):
+        if next(filter(lambda x:x["name"] == name ,items), None):   
             return { "Message" : "Item '{}' is already exist.".format(name) }, 400
 
         data = request.get_json()
@@ -20,10 +28,14 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self,name):
+        global items
+        items = list(filter(lambda x:x['name'] != name, items))
+        return {"Message" : "Item deleted"}
+
 class ItemList(Resource):
     def get(self):
         return{ "Items": items} 
-
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
